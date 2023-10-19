4075558374c8463dd6a1215b3409305f54a8df0c
Hrishikesh Jadhav <popeye@localhost.localdomain>
2023-10-19 15:46:55 +0530

    new

--- backend/settings.py
+++ backend/settings.py
@@ -1,4 +1,5 @@
 
+import os
 from pathlib import Path
 from decouple import config 
 
@@ -135,7 +136,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = ''
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
