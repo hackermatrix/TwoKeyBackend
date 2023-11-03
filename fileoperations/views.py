a49143c8f7b2e3888b6c0b5ed4f21b93c7c4b349
Hrishikesh Jadhav <popeye@localhost.localdomain>
2023-11-03 12:43:09 +0530

    dept endpt updated

--- fileoperations/views.py
+++ fileoperations/views.py
@@ -1,35 +1,48 @@
-from backend import settings
-from rest_framework.viewsets import GenericViewSet
-from rest_framework.views import APIView
-from backend.custom_perm_classes import *
-from rest_framework.permissions import IsAuthenticated
-from fileoperations.serializers import AccessLogSerializer, AllowedLocationSerializer, FileSerializer, SharedFileSerializer, SharedFilesRecepient
-from supabase import create_client, Client
-from decouple import config
-from rest_framework.response import Response
-from rest_framework import mixins
-from backend.supabase_auth import SupabaseAuthBackend
-from rest_framework import generics
-from .models import *
-from rest_framework import status
+from django.core import exceptions
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import Distance
-from django.core import exceptions
+from decouple import config
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.views import APIView
+from rest_framework import mixins, generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from backend import settings
+from backend.custom_perm_classes import SuperadminRequired, OrgadminRequired, OthersPerm
+from backend.supabase_auth import SupabaseAuthBackend
+
+from .models import *
+from fileoperations.serializers import (
+    AccessLogSerializer,
+    AllowedLocationSerializer,
+    FileSerializer,
+    SharedFileSerializer,
+    SharedFilesRecepient,
+)
+
+from supabase import create_client, Client
 
 
-class FileListing(mixins.ListModelMixin
-               ,generics.GenericAPIView):
-    
-    serializer_class= FileSerializer
+
+
+
+class FileListing(mixins.ListModelMixin, generics.GenericAPIView):
+    serializer_class = FileSerializer
     authentication_classes = [SupabaseAuthBackend]
     permission_classes = [OthersPerm]
     queryset = Objects.objects.all()
 
-    # List all files uploaded by all users from all departments. 
-    def get(self,request,*args,**kwargs):
-        self.queryset = Objects.objects.prefetch_related('owner').filter(owner__org=request.user.org).exclude(name='.emptyFolderPlaceholder')
-        return self.list(request,*args,**kwargs)
-    
+    # List all files uploaded by all users from all departments.
+    def get(self, request, *args, **kwargs):
+        self.queryset = (
+            Objects.objects.prefetch_related("owner")
+            .filter(owner__org=request.user.org)
+            .exclude(name=".emptyFolderPlaceholder")
+        )
+        return self.list(request, *args, **kwargs)
 
 
 class ExtraChecksMixin:
@@ -39,22 +52,30 @@ class ExtraChecksMixin:
             file_object = Objects.objects.get(id=file_id)
             file_owner = file_object.owner
         except Objects.DoesNotExist:
-            return Response({'error': "File not found"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "File not found"}, status=status.HTTP_400_BAD_REQUEST
+            )
         except exceptions.ValidationError:
-            return Response({'error': "Invalid Field Value"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Invalid Field Value"}, status=status.HTTP_400_BAD_REQUEST
+            )
         current_user = request.user
         if current_user == file_owner:
             return True
         return False
+
     # def is_location_valid(self,)
-    
+
+
 #  Below are the endpoints used at the Sender's side.
-class ShareViewSetSender(ExtraChecksMixin,
-                          mixins.RetrieveModelMixin, 
-                          mixins.CreateModelMixin, 
-                          mixins.DestroyModelMixin, 
-                          mixins.UpdateModelMixin,
-                          GenericViewSet):
+class ShareViewSetSender(
+    ExtraChecksMixin,
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    GenericViewSet,
+):
     authentication_classes = [SupabaseAuthBackend]
     serializer_class = SharedFileSerializer
     permission_classes = [OthersPerm]
@@ -62,36 +83,44 @@ class ShareViewSetSender(ExtraChecksMixin,
 
     # Delete a file share if the user is the owner of the file
     def destroy(self, request, *args, **kwargs):
-        file_id = kwargs.get('file')
+        file_id = kwargs.get("file")
         if self.check_file_ownership(request, file_id):
             return super().destroy(request, *args, **kwargs)
         else:
-            return Response({'error': "You cannot delete this share"}, status=status.HTTP_403_FORBIDDEN)
+            return Response(
+                {"error": "You cannot delete this share"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
     # Share a file (alias for create)
-    #Create a file share for list of UUIDs. 
-    # - Share a list of files  
+    # Create a file share for list of UUIDs.
+    # - Share a list of files
     # - Assigns a presigned url to the share as in the expiration_time parameter.
     # - Security Features not yet added.
     def share_file(self, request):
-        file_ids = request.data.get('file',[])
+        file_ids = request.data.get("file", [])
         shared_files = []
-        if(type(file_ids)!=list):
-            return Response({'error':"file should be a list"},status=status.HTTP_400_BAD_REQUEST)
+        if type(file_ids) != list:
+            return Response(
+                {"error": "file should be a list"}, status=status.HTTP_400_BAD_REQUEST
+            )
         for file_id in file_ids:
             if self.check_file_ownership(request, file_id):
-                request.data.pop('file')
-                request.data['file'] = file_id
+                request.data.pop("file")
+                request.data["file"] = file_id
                 serializer = self.get_serializer(data=request.data)
                 serializer.is_valid(raise_exception=True)
                 self.perform_create(serializer)
                 shared_files.append(file_id)
             else:
-                return Response({'error': "You cannot share this file"}, status=status.HTTP_403_FORBIDDEN)
+                return Response(
+                    {"error": "You cannot share this file"},
+                    status=status.HTTP_403_FORBIDDEN,
+                )
         headers = self.get_success_headers(serializer.data)
         response_data = {
-            "message":"Shares created SuccessFully!",
-            "shared_files":shared_files
+            "message": "Shares created SuccessFully!",
+            "shared_files": shared_files,
         }
         return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
 
@@ -101,141 +130,149 @@ class ShareViewSetSender(ExtraChecksMixin,
     #  - file_id
     #  - Shared_with list
     def get_file_info(self, request, *args, **kwargs):
-        self.lookup_field = 'file'
-        pk = kwargs.get('file')
+        self.lookup_field = "file"
+        pk = kwargs.get("file")
         res = self.retrieve(request)
         return res
 
-    # Delete All share of a file 
+    # Delete All share of a file
     # User file ownership check added already
     def delete_share(self, request, *args, **kwargs):
-        self.lookup_field = 'file'
-        pk = kwargs.get('file')
+        self.lookup_field = "file"
+        pk = kwargs.get("file")
         return self.destroy(request)
 
     # Edit file access
     # - The Access log entry is not implemented
-    def edit_access(self, request,**kwargs):
-        file_id = kwargs.get('file')
-        self.lookup_field = 'file'
-        if(self.check_file_ownership(request,file_id)):
-            return self.partial_update(request,**kwargs)
+    def edit_access(self, request, **kwargs):
+        file_id = kwargs.get("file")
+        self.lookup_field = "file"
+        if self.check_file_ownership(request, file_id):
+            return self.partial_update(request, **kwargs)
         else:
-            return Response({"error":"You cannot modify this share"},status=status.HTTP_401_UNAUTHORIZED)
+            return Response(
+                {"error": "You cannot modify this share"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
 
 # Below will be used at recepient's end
 
-class ShareViewSetReceiver(mixins.RetrieveModelMixin,
-                   mixins.CreateModelMixin,
-                   mixins.DestroyModelMixin,
-                   mixins.ListModelMixin,
-                   GenericViewSet):
-    
+
+class ShareViewSetReceiver(
+    mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
     serializer_class = SharedFilesRecepient
-    authentication_classes=[SupabaseAuthBackend]
+    authentication_classes = [SupabaseAuthBackend]
     permission_classes = [OthersPerm]
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         sec_obj = SecCheck.objects.get(shared=instance)
-        if(sec_obj.geo_enabled==None):
+        if sec_obj.geo_enabled == None:
             return super().retrieve(request, *args, **kwargs)
         else:
             try:
-                latitude = float(self.request.data.get('latitude', 0))
-                longitude = float(self.request.data.get('longitude', 0))
-                user_location = Point(latitude,longitude, srid=4326)
+                latitude = float(self.request.data.get("latitude", 0))
+                longitude = float(self.request.data.get("longitude", 0))
+                user_location = Point(latitude, longitude, srid=4326)
                 required_location = sec_obj.geo_enabled.location_point
-                distance_in_kms = user_location.distance(required_location)*100
-                if(distance_in_kms<=1):
+                distance_in_kms = user_location.distance(required_location) * 100
+                if distance_in_kms <= 1:
                     return super().retrieve(request, *args, **kwargs)
                 else:
-                    return Response({"error":"Location Not Allowed!"},status=status.HTTP_401_UNAUTHORIZED)  
+                    return Response(
+                        {"error": "Location Not Allowed!"},
+                        status=status.HTTP_401_UNAUTHORIZED,
+                    )
             except ValueError:
-                return Response({"error":"wrong parameter value"})
+                return Response({"error": "wrong parameter value"})
 
-
-    # Endpoint Serves recepient with the presigned url 
+    # Endpoint Serves recepient with the presigned url
     # - Adds an access log entry.
     def get_shared_file_url(self, request, *args, **kwargs):
         # Only files shared with current user
         self.queryset = SharedFiles.objects.filter(shared_with__id=request.user.id)
-        self.lookup_field = 'file'
-        res = self.retrieve(request,*args,**kwargs)
-        if res.status_code==200:
-            event_type="file_access"
-            file_id = kwargs.get('file')
+        self.lookup_field = "file"
+        res = self.retrieve(request, *args, **kwargs)
+        if res.status_code == 200:
+            event_type = "file_access"
+
+            file_id = kwargs.get("file")
             # Adding File Access Log
-            AccessLog.objects.create(user=request.user.id,file=file_id,event=event_type)
-        
+            AccessLog.objects.create(
+                user=request.user.id, file=file_id, event=event_type
+            )
+
         return res
-    
-     
-    def screen_shot_alert(self,request,*args,**kwargs):
+
+    def screen_shot_alert(self, request, *args, **kwargs):
         user_id = request.user.id
-        file_id = request.data.get('file_id')
+        file_id = request.data.get("file_id")
 
         try:
-            Objects.objects.filter(id=file_id)
-            SharedFiles.objects.get(shared_with__id=user_id,file_id=file_id)
+            Objects.objects.get(id=file_id)
+            SharedFiles.objects.get(shared_with__id=user_id, file_id=file_id)
 
             # Setting event type to "screenshot"
             event_type = "screenshot"
 
             # Create a new AccessLog entry with the event type "screenshot"
             AccessLog.objects.create(user=user_id, file=file_id, event=event_type)
-            return Response({'message': 'Screenshot event logged successfully'}, status=status.HTTP_201_CREATED)
+            return Response(
+                {"message": "Screenshot event logged successfully"},
+                status=status.HTTP_201_CREATED,
+            )
+        except exceptions.ValidationError:
+            return Response({"error":"wrong parameter value"},status=status.HTTP_400_BAD_REQUEST)
         except SharedFiles.DoesNotExist:
-            return Response({'error':"No Shares Exist!"})
+            return Response({"error": "invalid share"},status=status.HTTP_400_BAD_REQUEST)
         except Objects.DoesNotExist:
-            return Response({"error":"Relevent File Does not exist"})
-        
+            return Response({"error": "file does not exist"},status=status.HTTP_400_BAD_REQUEST)
+
     # Fetch the screen shot attempts for Current user's files
-    def get_screenshot_logs(self, request,*args,**kwargs):
+    def get_screenshot_logs(self, request, *args, **kwargs):
         self.serializer_class = AccessLogSerializer
         user = request.user
-        file_ids_owned_by_user = Objects.objects.filter(owner=user).values('id')
+        file_ids_owned_by_user = Objects.objects.filter(owner=user).values("id")
         print(file_ids_owned_by_user)
-        self.queryset = AccessLog.objects.filter(event='screenshot',file__in=file_ids_owned_by_user)
+        self.queryset = AccessLog.objects.filter(
+            event="screenshot", file__in=file_ids_owned_by_user
+        )
         return self.list(request)
-    
 
 
-class GeoLocationView(mixins.CreateModelMixin,
-                         mixins.ListModelMixin,
-                         GenericViewSet):
+class GeoLocationView(mixins.CreateModelMixin, mixins.ListModelMixin, GenericViewSet):
     permission_classes = [OrgadminRequired]
     authentication_classes = [SupabaseAuthBackend]
     serializer_class = AllowedLocationSerializer
     queryset = AllowedLocations.objects.all()
 
-    def perform_create(self,serializer):
+    def perform_create(self, serializer):
         try:
             current_user = self.request.user
             organization = current_user.org
             # Set the organization ID in the serializer before creating the entry.
             serializer.save(org=organization)
         except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
         return super().perform_create(serializer)
 
-    def create_location(self,request):
+    def create_location(self, request):
         print(request.data)
         return self.create(request)
-    
-    def get_locations(self,request):
+
+    def get_locations(self, request):
         user_org = request.user.org
         self.queryset = AllowedLocations.objects.filter(org=user_org)
         return self.list(request)
-    
+
     def get_permissions(self):
-        if self.action=="get_locations":
+        if self.action == "get_locations":
             self.permission_classes = [OthersPerm]
         return super().get_permissions()
-
-
-
-        
-
-
--- logic/views.py
+++ logic/views.py
@@ -7,10 +7,10 @@ from rest_framework.viewsets import GenericViewSet, ViewSet
 from rest_framework.views import APIView
 from rest_framework import mixins
 from rest_framework.response import Response
-from rest_framework import status 
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import generics
-from rest_framework.decorators import action,permission_classes
+from rest_framework.decorators import action, permission_classes
 
 from authenticate.models import Users
 from authenticate.serializers import UsersSerializer
@@ -21,130 +21,138 @@ from logic.serializers import *
 from rest_framework.request import Request
 
 
-
 # Organization ViewSet
-class OrgView(mixins.ListModelMixin,mixins.CreateModelMixin,GenericViewSet):
+class OrgView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
     authentication_classes = [SupabaseAuthBackend]
-    permission_classes=[SuperadminRequired]
+    permission_classes = [SuperadminRequired]
     serializer_class = OrganizationSerializer
-    
 
     def list_orgs(self, request, *args, **kwargs):
-        print("perm",self.permission_classes)
+        print("perm", self.permission_classes)
         self.queryset = Organizations.objects.all()
         return self.list(request, *args, **kwargs)
 
     def create_orgs(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
-    
+
     def get_permissions(self):
         if self.action == "list_orgs":
-            self.permission_classes=[AllowAny]
+            self.permission_classes = [AllowAny]
         return super().get_permissions()
-    
 
 
-
-    
 # Department ViewSet
-class DeptView(mixins.ListModelMixin,mixins.CreateModelMixin,GenericViewSet):
-    queryset = Departments.objects.all()    
+class DeptView(mixins.ListModelMixin, mixins.CreateModelMixin, GenericViewSet):
+    queryset = Departments.objects.all()
     # permission_classes=[OrgadminRequired]
-    authentication_classes=[SupabaseAuthBackend]
+    authentication_classes = [SupabaseAuthBackend]
     serializer_class = DepartmentSerializer
 
-    def list_depts(self,request,*args,**kwargs):
+    def list_depts(self, request, *args, **kwargs):
         org_id = request.user.org_id
         self.queryset = Departments.objects.filter(org=org_id)
-        return self.list(request,*args,**kwargs)
-    
+        return self.list(request, *args, **kwargs)
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         org_id = request.user.org
-        serializer.save(org=org_id)  
+        serializer.save(org=org_id)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
     def create_depts(self, request, *args, **kwargs):
-        return  self.create(request, *args, **kwargs)
-    
+        return self.create(request, *args, **kwargs)
+
     def get_permissions(self):
         if self.action == "list_depts":
-            self.permission_classes=[OrgadminRequired]
+            self.permission_classes = [OthersPerm]
         elif self.action == "create_depts":
             self.permission_classes = [OrgadminRequired]
         return super().get_permissions()
 
 
-
 # User ViewSet
-class UserViewSet(mixins.ListModelMixin,mixins.UpdateModelMixin,GenericViewSet):
+class UserViewSet(mixins.ListModelMixin, mixins.UpdateModelMixin, GenericViewSet):
     # serializer_class = UsersSerializer
     # queryset = Users.objects.all()
     permission_classes = [OrgadminRequired]
-    queryset = UserInfo.objects.all() 
+    queryset = UserInfo.objects.all()
     serializer_class = UserInfoSerializer
     authentication_classes = [SupabaseAuthBackend]
-    lookup_field = 'id'
+    lookup_field = "id"
 
     def list_users(self, request, *args, **kwargs):
         org_id = request.user.org_id
-        self.queryset = UserInfo.objects.filter(org=org_id).exclude(role_priv='org_admin').exclude(role_priv='super_admin').exclude(id=request.user.id)
+        self.queryset = (
+            UserInfo.objects.filter(org=org_id)
+            .exclude(role_priv="org_admin")
+            .exclude(role_priv="super_admin")
+            .exclude(id=request.user.id)
+        )
         # self.queryset = Users.objects.filter(org=org_id)
         return self.list(request, *args, **kwargs)
-    
+
     # Checking the Role's Existance
     def partial_update(self, request, *args, **kwargs):
-        serializer = RoleSerializer(data=request.data['role_priv'],partial=True)
+        serializer = RoleSerializer(data=request.data["role_priv"], partial=True)
         kk = serializer.is_valid()
 
-        role = request.data['role_priv']
+        role = request.data["role_priv"]
         try:
             Role.objects.get(role=role)
         except Role.DoesNotExist:
-            return Response({'error':"this role does not exist"},status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "this role does not exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
         except exceptions.ValidationError:
-            return Response({'error':"invalid value"},status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "invalid value"}, status=status.HTTP_400_BAD_REQUEST
+            )
         return super().partial_update(request, *args, **kwargs)
-    
-    def elevate(self,request,*args,**kwargs):
-        pk = kwargs.get('pk')
-        resp = self.partial_update(request,*args,**kwargs)
+
+    def elevate(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
+        resp = self.partial_update(request, *args, **kwargs)
         return resp
-    
+
     def get_permissions(self):
         if self.action == "list_users":
-            self.permission_classes=[OthersPerm]
+            self.permission_classes = [OthersPerm]
         return super().get_permissions()
-    
 
 
 # Roles Viewset
-class RolesViewset(mixins.ListModelMixin,
-                   mixins.CreateModelMixin,
-                   mixins.UpdateModelMixin,
-                   mixins.DestroyModelMixin,
-                   GenericViewSet):
+class RolesViewset(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    GenericViewSet,
+):
     authentication_classes = [SupabaseAuthBackend]
     queryset = Role.objects.all()
-    permission_classes=[SuperadminRequired]
-    serializer_class=RoleSerializer
-    lookup_field = 'id'
+    permission_classes = [SuperadminRequired]
+    serializer_class = RoleSerializer
+    lookup_field = "id"
 
-    def list_roles(self,request):
+    def list_roles(self, request):
         return self.list(request)
-    def create_roles(self,request):
+
+    def create_roles(self, request):
         return self.create(request)
+
     def update_roles(self, request):
         return self.update(request)
-    def delete_roles(self,request,*args,**kwargs):
-        kwargs.get('pk')
-        return self.destroy(request,*args,**kwargs)
-    
+
+    def delete_roles(self, request, *args, **kwargs):
+        kwargs.get("pk")
+        return self.destroy(request, *args, **kwargs)
+
     def get_permissions(self):
-        if self.action == 'list_roles':
+        if self.action == "list_roles":
             self.permission_classes = [OrgadminRequired]
         return super().get_permissions()
-    
