a04984502e8ece862c5183302cd7a7fa03886e6d
Hrishikesh Jadhav <popeye@localhost.localdomain>
2023-11-09 12:51:35 +0530

    manager added

--- logic/serializers.py
+++ logic/serializers.py
@@ -48,6 +48,6 @@ class NUserInfoSerializer(ModelSerializer):
         data['org'] = instance.org.name
         data['dept'] = instance.dept.name
         data['role_priv'] = instance.role_priv
-        data['manager'] = instance.manager
+        data['manager'] = instance.manager.name
         return data
     
