669f95f37c4245e273fcab20140b15d22ca383c7
Hrishikesh Jadhav <popeye@localhost.localdomain>
2024-02-01 17:20:58 +0530

    minor

--- backend/settings.py
+++ backend/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': config("SUPA_USER"),
         'PASSWORD': config("SUPA_PASS"),
         'HOST': config("SUPA_HOST"),
-        'PORT': '5432',
+        'PORT': config("SUPA_PORT"),
         'OPTIONS' : {
         'options': '-c search_path=public'
 },
